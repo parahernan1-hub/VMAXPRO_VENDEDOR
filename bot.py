08719bd0eb706e8a14714863d7a75e1c3dea6092
parahernan1-hub <parahernan1@gmail.com>
2025-12-29 21:01:03 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
 from flask import Flask, request
 
-app = Flask(_name_)
+app = Flask("vmaxpro")
 
 @app.route('/webhook', methods=['GET'])
 def verify():
-    token = "vmax2025"
-    if request.args.get("hub.verify_token") == token:
-        return request.args.get("hub.challenge")
-    return "Error de token", 403
+    return request.args.get("hub.challenge", "Error"), 200
 
 @app.route('/')
 def home():
-    return "Servidor de VmaxPro Activo"
+    return "Bot Online"
 
 if _name_ == "_main_":
     app.run(host='0.0.0.0', port=10000)
